460866a0dfa4f4778c029a2f0af4eaacc74c2bad
RT <ryusuke1133@gmail.com>
2022-10-11 08:03:06 +0000

    finish login, dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-
   root 'static_pages#top'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
 
